ee2d9697d5e48fd9d1edede0f57fe3842a37fc0b
thomas <thomas.dutreuilh@gmail.com>
2021-02-03 12:25:52 +0100

    added report link

--- frontend/components/reports_recap/reports_recap_component.rb
+++ frontend/components/reports_recap/reports_recap_component.rb
@@ -4,12 +4,14 @@ module ReportsRecapComponent
   extend ComponentHelper
 
   def headers
-    ['Date', 'Score mobile', 'Score desktop']
+    ['Date', 'Score mobile', 'Score desktop', '']
   end
 
   def cells report
     [humanized_report_time(report),
      c("google_lighthouse/score", score: report.score_mobile),
-     c("google_lighthouse/score", score: report.score_desktop)]
+     c("google_lighthouse/score", score: report.score_desktop),
+     link_to('Show report', lighthouse_report_path(report))
+    ]
   end
 end
