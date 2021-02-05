7323e2a30b2051934e7f9b114c929c589e471998
thomas <thomas.dutreuilh@gmail.com>
2021-02-05 16:06:09 +0100

    added id to reports

--- frontend/components/reports_recap/reports_recap_component.rb
+++ frontend/components/reports_recap/reports_recap_component.rb
@@ -4,11 +4,12 @@ module ReportsRecapComponent
   extend ComponentHelper
 
   def headers
-    ['Date', 'Score mobile', 'Score desktop', '']
+    ['Id','Date', 'Score mobile', 'Score desktop', '']
   end
 
   def cells report
-    [humanized_report_time(report),
+    [report.id,
+     humanized_report_time(report),
      c("google_lighthouse/score", score: report.score_mobile),
      c("google_lighthouse/score", score: report.score_desktop),
      link_to('Show report', lighthouse_report_path(report))
