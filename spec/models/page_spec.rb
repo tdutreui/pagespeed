2d9b8745101bdda6efb3700d15679a3303698637
thomas <thomas.dutreuilh@gmail.com>
2021-02-22 13:47:29 +0100

    tests - page - add score check

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -3,47 +3,60 @@ require 'rails_helper'
 RSpec.describe Page, type: :model do
   let(:valid_urls) { ['github.com', 'github.com/', 'http://github.com', 'https://github.com', 'https://github.com/hotwired', 'github.com/hotwired'] }
   let(:invalid_urls) { ['github', 'htp://github.com', 'https//github.com', 'ftp://github.com/hotwired', 'localhost'] }
-  let(:p) { build(:page) }
+  let(:page) { build(:page) }
 
   before do
     stub_pagespeed_api_client
   end
 
   it "should create a report on page create" do
-    p.save
-    expect(p.reports.count).to eq 1
+    page.save
+    expect(page.reports.count).to eq 1
   end
 
   it "should accept valid urls" do
 
     assert valid_urls.all? { |url|
-      p.url = url
-      p.valid?
+      page.url = url
+      page.valid?
     }
   end
 
   it "should reject invalid urls" do
     invalid_urls.none? { |url|
-      p.url = url
-      p.valid?
+      page.url = url
+      page.valid?
     }
   end
 
   it "should extract domain from valid url on validation" do
     valid_urls.each { |url|
-      p.url = url
-      p.valid?
-      expect(p.domain).to eq 'github.com'
+      page.url = url
+      page.valid?
+      expect(page.domain).to eq 'github.com'
     }
   end
 
   it "should extract path from valid url on validation" do
-    p.valid?
-    expect(p.path).to eq ""
+    page.valid?
+    expect(page.path).to eq ""
 
-    p.url = 'github.com/hotwired'
-    p.valid?
-    expect(p.path).to eq '/hotwired'
+    page.url = 'github.com/hotwired'
+    page.valid?
+    expect(page.path).to eq '/hotwired'
+  end
+
+  it "should return the most recent scores as page scores" do
+    page.save #creates a report on create
+    reports_count = page.reports.count
+
+    page.add_lighthouse_report
+    expect(page.reports.count).to eq(reports_count + 1)
+
+    last_report = page.reports.last
+    expect(last_report == LighthouseReport.order(:updated_at).last)
+    expect(page.score_mobile == last_report.score_mobile)
+    expect(page.score_desktop == last_report.score_desktop)
   end
 
 end
--- spec/support/lighthouse_reports_spec_helpers.rb
+++ spec/support/lighthouse_reports_spec_helpers.rb
@@ -1,7 +1,16 @@
 module LighthouseReportsSpecHelpers
   def stub_pagespeed_api_client
     response = JSON.parse(File.read('spec/support/stubs/sample_report.json'))
-    allow_any_instance_of(PagespeedApiClient).to receive(:run_pagespeed).and_return(response)
+
+    #randomize score
+    responses = []
+    10.times do
+      another_response = response.deep_dup
+      another_response['lighthouseResult']["categories"]["performance"]["score"] = rand.round(2)
+      responses << another_response
+    end
+
+    allow_any_instance_of(PagespeedApiClient).to receive(:run_pagespeed).and_return(*responses)
   end
 
   def stub_current_project_with current_project
