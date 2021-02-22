d8b07ccf1d09010012b9d70174d9b4443f793be8
thomas <thomas.dutreuilh@gmail.com>
2021-02-22 12:00:34 +0100

    tests - lighthouse_report.rb - 2

--- spec/models/lighthouse_report_spec.rb
+++ spec/models/lighthouse_report_spec.rb
@@ -6,20 +6,28 @@ RSpec.describe LighthouseReport, type: :model do
   end
 
   let(:page) { create(:page) }
+
   it "should add a report to newly created pages" do
     expect(page.reports.count).to eq 1
   end
 
-  it "should generate desktop and mobile lighthouse reports on creation" do
-    lighthouse_report = create(:lighthouse_report, page: page)
-    expect(lighthouse_report.json_report_mobile).to be_truthy
-    expect(lighthouse_report.json_report_desktop).to be_truthy
+  context "second report" do
+    let(:lighthouse_report) { create(:lighthouse_report, page: page) }
+
+    it "should generate desktop and mobile lighthouse reports on creation" do
+      expect(lighthouse_report.json_report_mobile).to be_truthy
+      expect(lighthouse_report.json_report_desktop).to be_truthy
+    end
+
+    it "should return reports as a Hash" do
+      expect(lighthouse_report.report('mobile').class).to eq Hash
+      expect(lighthouse_report.report('desktop').class).to eq Hash
+    end
+
+    it "should extract the scores" do
+      expect(lighthouse_report.score_mobile).to be_positive
+      expect(lighthouse_report.score_desktop).to be_positive
+    end
   end
 
-  it "should return reports as a Hash" do
-    lighthouse_report = create(:lighthouse_report, page: page)
-    expect(lighthouse_report.report('mobile').class).to eq Hash
-    expect(lighthouse_report.report('desktop').class).to eq Hash
-
-  end
 end
