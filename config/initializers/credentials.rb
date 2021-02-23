2ed1512b58599beb3f9ede72fe166aff66a51d90
thomas <thomas.dutreuilh@gmail.com>
2021-02-23 18:53:30 +0100

    disable plaintext credentials for prod

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -1,2 +1,4 @@
 #Load personal developer credentials
-Rails.application.credentials.config.merge!(YAML.load_file('config/local_credentials.yml').deep_symbolize_keys)
+if Rails.env.development?
+  Rails.application.credentials.config.merge!(YAML.load_file('config/local_credentials.yml').deep_symbolize_keys)
+end
